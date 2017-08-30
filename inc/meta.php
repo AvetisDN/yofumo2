4ee1dd7154de6d532d4a6cb2192f74e32f367788
games <C:\Users\games\AppData\Roaming\The Bat!\Mail>
2017-08-31 00:38:04 +0300

    Initial commit

---
+++ inc/meta.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+<meta name="robots" content="noodp">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta property="og:locale" content="en_US">
+<meta property="og:image" content="https://www.yofumo.com/assets/logo/logo_horizontal.png">
+<meta property="og:type" content="website">
+<meta property="og:site_name" content="Yofumo Technologies, Inc.">
+<link rel="home" href="https://www.yofumo.com">
+<link rel="shortcut icon" type="image/x-icon" href="/assets/logo/favicon.png"/>
+<link href="/assets/css/reset.css" rel="stylesheet">
+<link href="/assets/css/main.css" rel="stylesheet">
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "/inc/meta.php" ?>
+    <meta property="og:title" content="Yofumo">
+    <meta property="og:description" content="Yofumo leads the Revolution of Cannabis Storage and Processing.">
+    <meta property="og:url" content="https://www.yofumo.com/">
+    <title>Yofumo</title>
+</head>
+<body>
+
+
+<?php include "/inc/footer.php" ?>
+<?php include "/inc/scripts.php" ?>
+</body>
+</html>
