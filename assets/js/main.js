38ea9db1782f7fea2d8e4d0fc62f9c78fc73216c
AvetisDN <avetisdn@gmail.com>
2017-08-31 22:55:48 +0300

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 ;(function () {
 
-
+    $(document).ready(function () {
+        console.log(1);
+        $('.double-bounce1, .double-bounce2').addClass('hide');
+        setTimeout(function () {
+            $('.preloader').fadeOut();
+            $('.wrap').fadeIn();
+        },1000);
+    });
 
 })();
