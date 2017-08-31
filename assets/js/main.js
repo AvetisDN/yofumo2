f07512b70d5261b50782ffc7cae6afddcb8a07f4
AvetisDN <avetisdn@gmail.com>
2017-08-31 22:58:50 +0300

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 ;(function () {
 
     $(document).ready(function () {
-        console.log(1);
-        $('.double-bounce1, .double-bounce2').addClass('hide');
+        setTimeout(function () {
+            $('.double-bounce1, .double-bounce2').addClass('hide');
+        },1000);
         setTimeout(function () {
             $('.preloader').fadeOut();
             $('.wrap').fadeIn();
-        },1000);
+        },2000);
     });
 
 })();
