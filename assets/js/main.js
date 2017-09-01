9f963c5c70bf14e075e3a01a120b095b6ffb8242
AvetisDN <avetisdn@gmail.com>
2017-09-02 01:55:34 +0300

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,44 @@
 
     $(document).ready(function () {
         setTimeout(function () {
-            $('.double-bounce1, .double-bounce2').addClass('hide');
+            $('.logo-img, .logo-img2').addClass('hide');
         },2000);
         setTimeout(function () {
-            $('.preloader').fadeOut();
-            $('.wrap').fadeIn();
+            $('.logo-text').slideDown();
+            $('.logo-img img').fadeIn();
+            $('.scroll-icon').fadeIn();
+        },2000);
+        setTimeout(function () {
+            $('.header-menu').slideDown();
+        },2500);
+        setTimeout(function () {
+            $('.content').fadeIn(500);
+            particlesJS.load('particles-js', 'assets/js/particlesjs-config.json', function() {});
+            slider();
+            $(document).mousewheel(function () {
+                $('footer').addClass('show');
+            });
+            $('.scroll-icon').click(function () {
+                $('footer').addClass('show');
+            });
         },3000);
     });
+
+    var activeSlide = 0;
+
+    function slider() {
+        $('#slider figure.ani').removeClass('ani').fadeOut(300);
+        $('#slider figure').eq(activeSlide).fadeIn(300, function () {
+            $(this).addClass('ani');
+        });
+        $('.slider-navigation li.active').removeClass('active');
+        $('.slider-navigation li').eq(activeSlide).addClass('active');
+    }
+    $('.slider-navigation li').click(function () {
+        activeSlide = $(this).index();
+        slider();
+    });
+
+
 
 })();
