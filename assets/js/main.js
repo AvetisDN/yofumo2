2f7882d270ce1859d110129af07c7eb431bc8a5f
AvetisDN <avetisdn@gmail.com>
2017-10-15 02:33:24 +0300

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@
         activeSlide++;
         if(activeSlide===$('#slider figure').length) activeSlide=0;
         slider();
-    }, 7500);
+    }, 27500);
 
     function slider() {
         $('#slider figure.ani').removeClass('ani').fadeOut(300);
@@ -69,7 +69,7 @@
             activeSlide++;
             if(activeSlide===$('#slider figure').length) activeSlide=0;
             slider();
-        }, 7500);
+        }, 27500);
         slider();
     });
 
